c96578b0ea39469d09fd0e4b59e6c19bac3eace8
pedro558 <pedroafonso109@gmail.com>
2023-01-27 20:11:29 -0300

    :sparkles: feat: add infoWindow to marker

--- script.js
+++ script.js
@@ -4,14 +4,40 @@ function initMap() {
     zoom: 6,
     center: {lat: -22.9131524, lng: -44.0051082}
   });
-  
-  /*const marker = new google.maps.Marker({
-    position: novaIguacu,
-    map: map,
-    icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-  });
-  
-  
+    
+  function addMarker(props){
+    const marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      icon: props.iconImage,
+      content: props.content
+    })
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: props.content
+    })
+
+    marker.addListener('click', () => {
+      infoWindow.open(map, marker)
+    })
+  }
+
+  markers.map((marker) => {
+    addMarker(marker)
+  })
+}
+
+window.initMap = initMap;
+
+const markers = [
+  {
+    coords: {lat: -22.7580653, lng: -43.4496179},
+    content: '<h1>Nova Iguaçu</h1>'
+  }
+]
+
+
+/*
   const infoWindow = new google.maps.InfoWindow({
     content: '<h1>Nova Iguaçu, RJ</h1>'
   })
@@ -19,18 +45,3 @@ function initMap() {
   marker.addListener('click', () =>{
     infoWindow.open(map, marker)
   })*/
-
-  addMarker({lat: -22.7580653, lng: -43.4496179})
-
-  function addMarker(coords){
-    let marker = new google.maps.Marker({
-      position: coords,
-      map: map,
-      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-    })
-  }
-}
-
-
-
-window.initMap = initMap;
