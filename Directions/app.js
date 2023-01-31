4a72fada963ca22381cba52c5c2800c65e78aa23
pedro558 <pedroafonso109@gmail.com>
2023-01-31 18:51:39 -0300

    :sparkles: feat: config the map

---
+++ Directions/app.js
@@ -0,0 +1,17 @@
+// map options
+let myLatLng = {
+  lat: 38.3460,
+  lng: -0.4907
+}
+
+let mapOptions = {
+  center: myLatLng,
+  zoom: 6,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+}
+
+// createmap
+let map = new google.maps.Map(
+  document.getElementById('googleMap'),
+  mapOptions
+  )
