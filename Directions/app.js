f082a1f86f780ac07f3729fd83ce2167967da79f
pedro558 <pedroafonso109@gmail.com>
2023-02-10 14:34:17 -0300

    :construction: calcRoute function

--- Directions/app.js
+++ Directions/app.js
@@ -1,3 +1,7 @@
+// getting 'from' and 'to'
+const from = document.getElementById('from').value
+const to = document.getElementById('to').value
+
 // map options
 let myLatLng = {
   lat: 38.3460,
@@ -24,3 +28,29 @@ let directionsDisplay = new google.maps.DirectionsRenderer()
 
 // bind the directionsRenderer to the map
 directionsDisplay.setMap(map)
+
+
+function calcRoute(){
+  // create request
+  let request = {
+    origin: from,
+    destination: to,
+    travelMode: google.maps.TravelMode.DRIVING,
+    unitSystem: google.maps.UnitSystem.IMPERIAL
+  }
+
+   //pass request to route
+  directionsService.route(request, (result, status) =>{
+    if(status == google.maps.DirectionsStatus.Ok){
+      //get distance and time
+      const output = document.querySelector('#output')
+      output.innerHTML = `
+      <div class='alert-info'>
+      From: ${from}
+      To: ${to}
+      Driving Distance: ${result.routes[0].legs[0].distance.text}
+      Duration: ${result.routes[0].legs[0].duration.text}
+      `
+    }
+  })
+}
